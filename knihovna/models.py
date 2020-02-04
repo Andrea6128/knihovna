c5878a6dd2fe91a793051d3d72221bb12dd7914f
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-02-04 16:59:37 +0100

    fixed models.py

--- knihovna/models.py
+++ knihovna/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
     authorName = models.CharField(max_length=100, verbose_name=('Author\'s name'))
     authorDetail = models.TextField(max_length=1000, verbose_name=('Author description'))
 
-    # get author url by id for use in teplates
+    # get author url by id for use in templates
     def getAuthorUrl(self):
         return reverse('author_detail_view', kwargs={'id': self.id})
 
@@ -25,7 +25,7 @@ class Book(models.Model):
     borrowedDate = models.DateField(blank=True, null=True, verbose_name=('Borrowed on'))
     returnDate = models.DateField(blank=True, null=True, verbose_name=('Will be returned on'))
 
-    # get book url by id for use in teplates
+    # get book url by id for use in templates
     def getBookUrl(self):
         return reverse('book_detail_view', kwargs={'id': self.id})
 
